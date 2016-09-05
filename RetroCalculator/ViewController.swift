a4433e6135d6db66dc711d8c70a9def3bf6e4a9d
Amir Bahadori <pbahadori156@gmail.com>
2016-09-04 18:52:30 -0700

    Implemented calculator and clear functionality along with a fun looking launch screen

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -11,7 +11,22 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var outputLabel: UILabel!
     var btnSound: AVAudioPlayer!
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "="
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,12 +51,45 @@ class ViewController: UIViewController {
             
             // We loaded it into the URL, the sound itself, how do we play the sound? HAVE THE BUTTONS CALL A FUNCTION
         }
+        outputLabel.text = "0"
         
     }
 
     @IBAction func numberPressed(sender: UIButton) {
       playSound()
+        
+        // The += allows us to add numbers next to each other like we'd do in a calculator, if it was just = we couldn't have multiple numbers on the label
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
     }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    
+    @IBAction func onClearPressed(_ sender: AnyObject) {
+       currentOperation = Operation.Empty
+        outputLabel.text = "0.0"
+        runningNumber = ""
+        
+    }
+   
     
     func playSound() {
         // Allows us to tap the button multiple times quickly and make the sound quickly with btnSound.stop
@@ -49,10 +97,42 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
-        
     }
     // This button sound lives in our project somewhere when the app is running on someone's phone. There is an actual URL to that path and it's stored somewhere on the user's device.
 
-
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            
+            //A user selected an operator, but then selected another operator without first entering a number
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    
+                } else if currentOperation ==  Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                    
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                    
+                }
+                
+                leftValStr = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+            //This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 }
 
